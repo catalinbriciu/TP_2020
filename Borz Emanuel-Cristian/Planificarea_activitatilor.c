0484998c38b5fb1ccf6137f7d2508fe534a90175
cr1ssprojects <62558159+cr1ssprojects@users.noreply.github.com>
2020-05-31 23:06:08 +0300

    Update Planificarea_activitatilor.c

--- Borz Emanuel-Cristian/Planificarea_activitatilor.c
+++ Borz Emanuel-Cristian/Planificarea_activitatilor.c
@@ -1,45 +1,55 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
+#include<Bits.h>
 #pragma warning (disable:4996)
 
-int n,i,cost[10], value[10],weight,current_weight,maxi,used[10];
-float total_value;
+typedef struct activ {
+	int id, start, end;
+}activity;
+
+activity a[20];
+int n,m, s[20];
+
 void read()
-{   printf("Nr of objects: ");
+{
+	int i;
+	printf("Number of planned activities: ");
 	scanf("%d", &n);
 	for (i = 1; i <= n; i++) {
-		printf("Object #%d cost: ", i);
-		scanf("%d", &cost[i]);
-		printf("Object #%d value: ", i);
-		scanf("%d", &value[i]);
+		printf("Activity #%d start date:", i);
+		scanf("%d", &a[i].start);
+		printf("Activity #%d end date:", i);
+		scanf("%d", &a[i].end);
+		a[i].id = i; // numarul activitatii
 	}
-	printf("Maximum weight: ");
-	scanf("%d", &weight);
 }
-int main() {
-	read();
-	for (i = 1; i <=n; i++)
-		used[i] = 0; 
+void sort()
+{
+	int i, j; activity aux;
+	for (i = 1; i < n; i++)
+		for (j = i; j <= n; j++)
+			if (a[i].end >= a[i].start) {
+				aux = a[i];
+				a[i] = a[j];
+				a[j] = aux;
+			}
+}
+void solve()
+{
+	int i, j;  i = 0;
+	printf("Activity %d \n", i);
 
-	current_weight = weight;
-	while (current_weight > 0) { 
-		maxi = -1;
-		for (i = 0; i < n; ++i)
-			if ((used[i] == 0) &&
-				((maxi == -1) || ((float)value[i] / cost[i] > (float)value[maxi] / cost[maxi])))
-				maxi = i;
-
-		used[maxi] = 1; 
-		current_weight -= cost[maxi]; 
-		total_value += value[maxi];
-		if (current_weight >= 0)
-			printf("Added object %d (%d$, %dKg) completely in the bag. Space left: %d.\n", maxi + 1, value[maxi], cost[maxi], current_weight);
-		else {
-			printf("Added %d%% (%d$, %dKg) of object %d in the bag.\n", (int)((1 + (float)current_weight / cost[maxi]) * 100), value[maxi], cost[maxi], maxi + 1);
-			total_value -= value[maxi];
-			total_value += (1 + (float)current_weight / cost[maxi]) * value[maxi];
-		}
+	for (j = 1; j < n; j++){
+			printf(" Activity %d \n", j);
+			i = j;
 	}
+}
 
-	printf("Filled the bag with objects worth %.2f$.\n", total_value);
+
+int main(){
+	read();
+	sort();
+	solve();
+	//print();
+	return 0;
 }
